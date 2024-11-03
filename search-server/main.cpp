1d1be1d175b10e4f8449888d69f6cd3734879790
ChernyshevYV <107913053+NutcrackerCom@users.noreply.github.com>
2024-11-03 23:06:39 +0300

    Update main.cpp

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -10,26 +10,25 @@
 #include <vector>
 #include <cstdint>
 
-using namespace std;
-
+using namespace std::string_literals;
 const int MAX_RESULT_DOCUMENT_COUNT = 5;
 
-string ReadLine() {
-    string s;
-    getline(cin, s);
+std::string ReadLine() {
+	std::string s;
+	std::getline(std::cin, s);
     return s;
 }
 
 int ReadLineWithNumber() {
     int result;
-    cin >> result;
+    std::cin >> result;
     ReadLine();
     return result;
 }
 
-vector<string> SplitIntoWords(const string& text) {
-    vector<string> words;
-    string word;
+std::vector<std::string> SplitIntoWords(const std::string& text) {
+	std::vector<std::string> words;
+	std::string word;
     for (const char c : text) {
         if (c == ' ') {
             if (!word.empty()) {
@@ -61,8 +60,8 @@ struct Document {
     int rating = 0;
 };
 
-ostream& operator<<(ostream& out, const Document& document) {
-    out << "{ "s
+std::ostream& operator<<(std::ostream& out, const Document& document) {
+    out << "{ "
         << "document_id = "s << document.id << ", "s
         << "relevance = "s << document.relevance << ", "s
         << "rating = "s << document.rating << " }"s;
@@ -70,9 +69,9 @@ ostream& operator<<(ostream& out, const Document& document) {
 }
 
 template <typename StringContainer>
-set<string> MakeUniqueNonEmptyStrings(const StringContainer& strings) {
-    set<string> non_empty_strings;
-    for (const string& str : strings) {
+std::set<std::string> MakeUniqueNonEmptyStrings(const StringContainer& strings) {
+	std::set<std::string> non_empty_strings;
+    for (const std::string& str : strings) {
         if (!str.empty()) {
             non_empty_strings.insert(str);
         }
@@ -94,25 +93,25 @@ public:
         : stop_words_(MakeUniqueNonEmptyStrings(stop_words))  // Extract non-empty stop words
     {
         if (!all_of(stop_words_.begin(), stop_words_.end(), IsValidWord)) {
-            throw invalid_argument("Some of stop words are invalid"s);
+            throw std::invalid_argument("Some of stop words are invalid"s);
         }
     }
 
-    explicit SearchServer(const string& stop_words_text)
+    explicit SearchServer(const std::string& stop_words_text)
         : SearchServer(SplitIntoWords(stop_words_text))  // Invoke delegating constructor
                                                          // from string container
     {
     }
 
-    void AddDocument(int document_id, const string& document, DocumentStatus status,
-                     const vector<int>& ratings) {
+    void AddDocument(int document_id, const std::string& document, DocumentStatus status,
+                     const std::vector<int>& ratings) {
         if ((document_id < 0) || (documents_.count(document_id) > 0)) {
-            throw invalid_argument("Invalid document_id"s);
+            throw std::invalid_argument("Invalid document_id"s);
         }
         const auto words = SplitIntoWordsNoStop(document);
 
         const double inv_word_count = 1.0 / words.size();
-        for (const string& word : words) {
+        for (const std::string& word : words) {
             word_to_document_freqs_[word][document_id] += inv_word_count;
         }
         documents_.emplace(document_id, DocumentData{ComputeAverageRating(ratings), status});
@@ -120,13 +119,13 @@ public:
     }
 
     template <typename DocumentPredicate>
-    vector<Document> FindTopDocuments(const string& raw_query,
+    std::vector<Document> FindTopDocuments(const std::string& raw_query,
                                       DocumentPredicate document_predicate) const {
         const auto query = ParseQuery(raw_query);
 
         auto matched_documents = FindAllDocuments(query, document_predicate);
 
-        sort(matched_documents.begin(), matched_documents.end(),
+        std::sort(matched_documents.begin(), matched_documents.end(),
              [](const Document& lhs, const Document& rhs) {
                  return lhs.relevance > rhs.relevance
                      || (abs(lhs.relevance - rhs.relevance) < 1e-6 && lhs.rating > rhs.rating);
@@ -138,14 +137,14 @@ public:
         return matched_documents;
     }
 
-    vector<Document> FindTopDocuments(const string& raw_query, DocumentStatus status) const {
+    std::vector<Document> FindTopDocuments(const std::string& raw_query, DocumentStatus status) const {
         return FindTopDocuments(
             raw_query, [status](int document_id, DocumentStatus document_status, int rating) {
                 return document_status == status;
             });
     }
 
-    vector<Document> FindTopDocuments(const string& raw_query) const {
+    std::vector<Document> FindTopDocuments(const std::string& raw_query) const {
         return FindTopDocuments(raw_query, DocumentStatus::ACTUAL);
     }
 
@@ -157,12 +156,12 @@ public:
         return document_ids_.at(index);
     }
 
-    tuple<vector<string>, DocumentStatus> MatchDocument(const string& raw_query,
+    std::tuple<std::vector<std::string>, DocumentStatus> MatchDocument(const std::string& raw_query,
                                                         int document_id) const {
         const auto query = ParseQuery(raw_query);
 
-        vector<string> matched_words;
-        for (const string& word : query.plus_words) {
+        std::vector<std::string> matched_words;
+        for (const std::string& word : query.plus_words) {
             if (word_to_document_freqs_.count(word) == 0) {
                 continue;
             }
@@ -170,7 +169,7 @@ public:
                 matched_words.push_back(word);
             }
         }
-        for (const string& word : query.minus_words) {
+        for (const std::string& word : query.minus_words) {
             if (word_to_document_freqs_.count(word) == 0) {
                 continue;
             }
@@ -187,27 +186,27 @@ private:
         int rating;
         DocumentStatus status;
     };
-    const set<string> stop_words_;
-    map<string, map<int, double>> word_to_document_freqs_;
-    map<int, DocumentData> documents_;
-    vector<int> document_ids_;
+    const std::set<std::string> stop_words_;
+    std::map<std::string, std::map<int, double>> word_to_document_freqs_;
+    std::map<int, DocumentData> documents_;
+    std::vector<int> document_ids_;
 
-    bool IsStopWord(const string& word) const {
+    bool IsStopWord(const std::string& word) const {
         return stop_words_.count(word) > 0;
     }
 
-    static bool IsValidWord(const string& word) {
+    static bool IsValidWord(const std::string& word) {
         // A valid word must not contain special characters
         return none_of(word.begin(), word.end(), [](char c) {
             return c >= '\0' && c < ' ';
         });
     }
 
-    vector<string> SplitIntoWordsNoStop(const string& text) const {
-        vector<string> words;
-        for (const string& word : SplitIntoWords(text)) {
+    std::vector<std::string> SplitIntoWordsNoStop(const std::string& text) const {
+    	std::vector<std::string> words;
+        for (const std::string& word : SplitIntoWords(text)) {
             if (!IsValidWord(word)) {
-                throw invalid_argument("Word "s + word + " is invalid"s);
+                throw std::invalid_argument("Word "s + word + "s is invalid"s);
             }
             if (!IsStopWord(word)) {
                 words.push_back(word);
@@ -216,7 +215,7 @@ private:
         return words;
     }
 
-    static int ComputeAverageRating(const vector<int>& ratings) {
+    static int ComputeAverageRating(const std::vector<int>& ratings) {
         int rating_sum = 0;
         for (const int rating : ratings) {
             rating_sum += rating;
@@ -225,36 +224,36 @@ private:
     }
 
     struct QueryWord {
-        string data;
+    	std::string data;
         bool is_minus;
         bool is_stop;
     };
 
-    QueryWord ParseQueryWord(const string& text) const {
+    QueryWord ParseQueryWord(const std::string& text) const {
         if (text.empty()) {
-            throw invalid_argument("Query word is empty"s);
+            throw std::invalid_argument("Query word is empty"s);
         }
-        string word = text;
+        std::string word = text;
         bool is_minus = false;
         if (word[0] == '-') {
             is_minus = true;
             word = word.substr(1);
         }
         if (word.empty() || word[0] == '-' || !IsValidWord(word)) {
-            throw invalid_argument("Query word "s + text + " is invalid");
+            throw std::invalid_argument("Query word "s + text + " is invalid"s);
         }
 
         return {word, is_minus, IsStopWord(word)};
     }
 
     struct Query {
-        set<string> plus_words;
-        set<string> minus_words;
+    	std::set<std::string> plus_words;
+    	std::set<std::string> minus_words;
     };
 
-    Query ParseQuery(const string& text) const {
+    Query ParseQuery(const std::string& text) const {
         Query result;
-        for (const string& word : SplitIntoWords(text)) {
+        for (const std::string& word : SplitIntoWords(text)) {
             const auto query_word = ParseQueryWord(word);
             if (!query_word.is_stop) {
                 if (query_word.is_minus) {
@@ -268,15 +267,15 @@ private:
     }
 
     // Existence required
-    double ComputeWordInverseDocumentFreq(const string& word) const {
-        return log(GetDocumentCount() * 1.0 / word_to_document_freqs_.at(word).size());
+    double ComputeWordInverseDocumentFreq(const std::string& word) const {
+        return std::log(GetDocumentCount() * 1.0 / word_to_document_freqs_.at(word).size());
     }
 
     template <typename DocumentPredicate>
-    vector<Document> FindAllDocuments(const Query& query,
+    std::vector<Document> FindAllDocuments(const Query& query,
                                       DocumentPredicate document_predicate) const {
-        map<int, double> document_to_relevance;
-        for (const string& word : query.plus_words) {
+    	std::map<int, double> document_to_relevance;
+        for (const std::string& word : query.plus_words) {
             if (word_to_document_freqs_.count(word) == 0) {
                 continue;
             }
@@ -289,7 +288,7 @@ private:
             }
         }
 
-        for (const string& word : query.minus_words) {
+        for (const std::string& word : query.minus_words) {
             if (word_to_document_freqs_.count(word) == 0) {
                 continue;
             }
@@ -298,7 +297,7 @@ private:
             }
         }
 
-        vector<Document> matched_documents;
+        std::vector<Document> matched_documents;
         for (const auto &[document_id, relevance] : document_to_relevance) {
             matched_documents.push_back(
                 {document_id, relevance, documents_.at(document_id).rating});
@@ -308,54 +307,54 @@ private:
 };
 
 void PrintDocument(const Document& document) {
-    cout << "{ "s
+	std::cout << "{ "s
          << "document_id = "s << document.id << ", "s
          << "relevance = "s << document.relevance << ", "s
-         << "rating = "s << document.rating << " }"s << endl;
+         << "rating = "s << document.rating << " }"s << std::endl;
 }
 
-void PrintMatchDocumentResult(int document_id, const vector<string>& words, DocumentStatus status) {
-    cout << "{ "s
+void PrintMatchDocumentResult(int document_id, const std::vector<std::string>& words, DocumentStatus status) {
+	std::cout << "{ "s
          << "document_id = "s << document_id << ", "s
          << "status = "s << static_cast<int>(status) << ", "s
          << "words ="s;
-    for (const string& word : words) {
-        cout << ' ' << word;
+    for (const std::string& word : words) {
+    	std::cout << ' ' << word;
     }
-    cout << "}"s << endl;
+    std::cout << "}"s << std::endl;
 }
 
-void AddDocument(SearchServer& search_server, int document_id, const string& document,
-                 DocumentStatus status, const vector<int>& ratings) {
+void AddDocument(SearchServer& search_server, int document_id, const std::string& document,
+                 DocumentStatus status, const std::vector<int>& ratings) {
     try {
         search_server.AddDocument(document_id, document, status, ratings);
-    } catch (const exception& e) {
-        cout << "Error in adding document "s << document_id << ": "s << e.what() << endl;
+    } catch (const std::exception& e) {
+    	std::cout << "Error in adding document "s << document_id << ": "s << e.what() << std::endl;
     }
 }
 
-void FindTopDocuments(const SearchServer& search_server, const string& raw_query) {
-    cout << "Results for request: "s << raw_query << endl;
+void FindTopDocuments(const SearchServer& search_server, const std::string& raw_query) {
+	std::cout << "Results for request: "s << raw_query << std::endl;
     try {
         for (const Document& document : search_server.FindTopDocuments(raw_query)) {
             PrintDocument(document);
         }
-    } catch (const exception& e) {
-        cout << "Error is seaching: "s << e.what() << endl;
+    } catch (const std::exception& e) {
+    	std::cout << "Error is seaching: "s << e.what() << std::endl;
     }
 }
 
-void MatchDocuments(const SearchServer& search_server, const string& query) {
+void MatchDocuments(const SearchServer& search_server, const std::string& query) {
     try {
-        cout << "Matching for request: "s << query << endl;
+    	std::cout << "Matching for request: "s << query << std::endl;
         const int document_count = search_server.GetDocumentCount();
         for (int index = 0; index < document_count; ++index) {
             const int document_id = search_server.GetDocumentId(index);
             const auto [words, status] = search_server.MatchDocument(query, document_id);
             PrintMatchDocumentResult(document_id, words, status);
         }
-    } catch (const exception& e) {
-        cout << "Error in matchig request "s << query << ": "s << e.what() << endl;
+    } catch (const std::exception& e) {
+    	std::cout << "Error in matchig request "s << query << ": "s << e.what() << std::endl;
     }
 }
 
@@ -386,7 +385,7 @@ private:
 };
 
 template <typename Iterator>
-ostream& operator<<(ostream& out, const IteratorRange<Iterator>& range) {
+std::ostream& operator<<(std::ostream& out, const IteratorRange<Iterator>& range) {
     for (Iterator it = range.begin(); it != range.end(); ++it) {
         out << *it;
     }
@@ -398,7 +397,7 @@ class Paginator {
 public:
     Paginator(Iterator begin, Iterator end, size_t page_size) {
         for (size_t left = distance(begin, end); left > 0;) {
-            const size_t current_page_size = min(page_size, left);
+            const size_t current_page_size = std::min(page_size, left);
             const Iterator current_page_end = next(begin, current_page_size);
             pages_.push_back({begin, current_page_end});
 
@@ -420,7 +419,7 @@ public:
     }
 
 private:
-    vector<IteratorRange<Iterator>> pages_;
+    std::vector<IteratorRange<Iterator>> pages_;
 };
 
 template <typename Container>
@@ -438,19 +437,19 @@ public:
 
     // сделаем "обертки" для всех методов поиска, чтобы сохранять результаты для нашей статистики
     template <typename DocumentPredicate>
-    vector<Document> AddFindRequest(const string& raw_query, DocumentPredicate document_predicate) {
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
         const auto result = search_server_.FindTopDocuments(raw_query, document_predicate);
         AddRequest(result.size());
         return result;
     }
 
-    vector<Document> AddFindRequest(const string& raw_query, DocumentStatus status) {
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status) {
         const auto result = search_server_.FindTopDocuments(raw_query, status);
         AddRequest(result.size());
         return result;
     }
 
-    vector<Document> AddFindRequest(const string& raw_query) {
+    std::vector<Document> AddFindRequest(const std::string& raw_query) {
         const auto result = search_server_.FindTopDocuments(raw_query);
         AddRequest(result.size());
         return result;
@@ -465,7 +464,7 @@ private:
         uint64_t timestamp;
         int results;
     };
-    deque<QueryResult> requests_;
+    std::deque<QueryResult> requests_;
     const SearchServer& search_server_;
     int no_results_requests_;
     uint64_t current_time_;
@@ -509,5 +508,5 @@ int main() {
     request_queue.AddFindRequest("big collar"s);
     // первый запрос удален, 1437 запросов с нулевым результатом
     request_queue.AddFindRequest("sparrow"s);
-    cout << "Total empty requests: "s << request_queue.GetNoResultRequests() << endl;
+    std::cout << "Total empty requests: "s << request_queue.GetNoResultRequests() << std::endl;
 }
